6eb147e7142301451656329eaec9f65f097008e8
MiguelHoz win <miguel.hoz@gmail.com>
2015-07-05 22:02:17 +0200

    Revert "Revert "Agregamos créditos y mas:""
    This reverts commit 57dc4c6ba17a420f4bb82ab3f1261eb727ab2fe2.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,24 @@
-
 //GET /quizes/question
-exports.question = function (req, res) {
+exports.question = function(req, res) {
     res.render('quizes/question', {
-                             title: vTitulo
-                            ,pregunta: 'Capital de Italia'
-                            });
+        title: vTitulo,
+        pregunta: 'Capital of Italy'
+    });
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res) {
     var vResultado;
-    if (req.query.respuesta.toUpperCase() === 'ROMA') {
-        vResultado = 'Correcto';
+    if (req.query.respuesta.toUpperCase() === 'ROMA' ||
+        req.query.respuesta.toUpperCase() === 'ROME') {
+        vResultado = 'Correct';
     } else {
-        vResultado = 'Incorrecto';
+        vResultado = 'Wrong';
     }
 
     res.render('quizes/answer', {
-                title: vTitulo
-               ,resultado: vResultado
-               ,respuesta: req.query.respuesta
-            });
+        title: vTitulo,
+        resultado: vResultado,
+        respuesta: req.query.respuesta
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: vTitulo });
 });
 
+router.get('/author', function(req,res) {
+    res.render('author', {
+        title: vTitulo,
+        author: vAuthorName,
+        nick: vAuthorNick,
+        imagen: vAuthorImg
+        });
+})
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
